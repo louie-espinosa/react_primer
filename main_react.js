7e4cebee31e031e95707309c238d428b6dd27f20
Louie Espinosa <louie.espinosa@proton.me>
2022-12-09 00:42:05 -0600

    created a new project for react; added some basic code to test

---
+++ main_react.js
@@ -0,0 +1,8 @@
+let JSX = <h1>Hello JSX!</h1>;
+
+JSX = <div><h1>Hello JSX</h1><p></p><ul><li></li><li></li><li></li></ul></div>
+JSX = <div>
+    {/*This is a comment */}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+</div>
