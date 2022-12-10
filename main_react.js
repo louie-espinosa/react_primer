166f7b2d7b740a4dcc782755c133737a07b1dfe4
Louie Espinosa <louie.espinosa@proton.me>
2022-12-09 22:33:41 -0600

    Added index.html, redeclared variable, attempted to render, but failed.  spending more time on research

--- main_react.js
+++ main_react.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 let JSX = <h1>Hello JSX!</h1>;
 
 JSX = <div><h1>Hello JSX</h1><p></p><ul><li></li><li></li><li></li></ul></div>
@@ -6,3 +9,11 @@ JSX = <div>
     <h1>This is a block of JSX</h1>
     <p>Here's a subtitle</p>
 </div>
+
+JSX = <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+</div>
+
+const cNode = ReactDOM.createRoot(document.getElementById('challenge-node'));
+cNode.render(JSX);
