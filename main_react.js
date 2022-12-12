c4b62c4adbdd8aeda2a175e55534b7b78fa5f988
Louie Espinosa <louie.espinosa@proton.me>
2022-12-11 21:07:08 -0600

    Added variable and succesfully ran and rendered the code in React app

--- main_react.js
+++ main_react.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 
-let JSX = <h1>Hello JSX!</h1>;
 
-JSX = <div><h1>Hello JSX</h1><p></p><ul><li></li><li></li><li></li></ul></div>
-JSX = <div>
-    {/*This is a comment */}
-    <h1>This is a block of JSX</h1>
-    <p>Here's a subtitle</p>
-</div>
+// let JSX = (
+//     <h1>Hello JSX!</h1>
+// );
+//
+// JSX = <div><h1>Hello JSX</h1><p></p><ul><li></li><li></li><li></li></ul></div>
+//
+// JSX = (
+//     <div>
+//     {/*This is a comment */}
+//     <h1>This is a block of JSX</h1>
+//     <p>Here's a subtitle</p>
+//     </div>
+// );
 
-JSX = <div>
-    <h1>Hello World</h1>
-    <p>Lets render this to the DOM</p>
-</div>
-
-const cNode = ReactDOM.createRoot(document.getElementById('challenge-node'));
-cNode.render(JSX);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const test = (
+        <div>
+            <h1>Hello World</h1>
+            <p>Lets render this to the DOM</p>
+        </div>
+    );
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(test);
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+  //     </header>
+  //   </div>
+  //
+  // );
 }
 
 export default App;
